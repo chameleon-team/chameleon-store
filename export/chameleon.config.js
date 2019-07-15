d8e9f378374b15e803095d6e35fb15cefd399d0c
huyuangang <huyuangang@didichuxing.com>
2019-07-15 14:35:42 +0800

    自动构建脚本

---
+++ export/chameleon.config.js
@@ -0,0 +1,122 @@
+
+const path = require('path');
+
+cml.config.merge({
+  templateLang: "cml",
+  templateType: "smarty",
+  enableGlobalCheck: false,
+  wx: {
+    dev: {
+    },
+    build: {
+    },
+    export: {
+      entry: ['index.js'],
+      publicPath: '/',
+      hash: false,
+      externals: {
+        'mobx': 'mobx',
+        'vuex': 'vuex'
+      }
+    }
+  },
+  alipay: {
+    dev: {
+    },
+    build: {
+    },
+    export: {
+      entry: ['index.js'],
+      publicPath: '/',
+      hash: false,
+      externals: {
+        'mobx': 'mobx',
+        'vuex': 'vuex'
+      }
+    }
+  },
+  baidu: {
+    dev: {
+    },
+    build: {
+    },
+    export: {
+      entry: ['index.js'],
+      publicPath: '/',
+      hash: false,
+      externals: {
+        'mobx': 'mobx',
+        'vuex': 'vuex'
+      }
+    }
+  },
+  web: {
+    dev: {
+      analysis: false,
+      console: false,
+      babelPolyfill: true
+    },
+    build: {
+      analysis: false,
+      babelPolyfill: true
+    },
+    export: {
+      entry: ['index.js'],
+      publicPath: '/',
+      hash: false,
+      externals: {
+        'mobx': 'mobx',
+        'vuex': 'vuex'
+      }
+    }
+  },
+  weex: {
+    dev: {
+    },
+    build: {
+    },
+    export: {
+      entry: ['index.js'],
+      publicPath: '/',
+      outputPath: path.resolve(__dirname, 'dist/weex'),
+      hash: false,
+      externals: {
+        'mobx': 'mobx',
+        'vuex': 'vuex'
+      }
+    }
+  }
+})
+
+cml.utils.plugin('webpackConfig', function({ type, media, webpackConfig }, cb) {
+  // cb函数用于设置修改后的配置
+  
+  if (['wx', 'alipay', 'baidu'].indexOf(type) !== -1) {
+    delete webpackConfig.target;
+    delete webpackConfig.output.jsonpFunction;
+    webpackConfig.output.libraryTarget = 'umd';
+    webpackConfig.output.filename = '[name].js';
+    let index  = webpackConfig.plugins.findIndex(item => item.constructor.name === 'CommonsChunkPlugin')
+    webpackConfig.plugins.splice(index, 1);
+  }
+  webpackConfig.entry = {
+    'index': path.resolve(__dirname, 'index.js')
+  }
+  webpackConfig.output.path = path.resolve(__dirname, `dist/${type}`)
+  webpackConfig.module.rules.push({
+    test: /\.js$/,
+    // 不能babel babel-runtime
+    include: [path.join(cml.projectRoot, 'src'), path.join(cml.projectRoot, 'index.js')],
+    use: [{
+      loader: 'babel-loader',
+      options: {
+        'filename': path.join(cml.root, 'chameleon.js')
+      }
+    }]
+  });
+  cb({
+    type,
+    media,
+    webpackConfig
+  });
+});
